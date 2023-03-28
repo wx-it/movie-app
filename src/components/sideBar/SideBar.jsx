c430d40f4de475b12f4555687a30e7d6f603257f
Lix <zinebbendjafer9@gmail.com>
2023-03-28 23:50:40 +0100

    sidebar done

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,6 +1,9 @@
 import "./sideBar.css";
 import { useState } from "react";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { SlScreenDesktop } from "react-icons/sl";
+import { TbMovie } from "react-icons/tb";
+import { AiOutlineHome, AiOutlineFire } from "react-icons/ai";
 const SideBar = () => {
   const sideBarCollapse = localStorage.getItem("sideBar-collapsed");
   const [isExpanded, setIsExpanded] = useState(sideBarCollapse ? false : true);
@@ -17,10 +20,32 @@ const SideBar = () => {
 
   return (
     <div className={isExpanded ? "sideBar" : "sideBar collapsed"}>
-      <MdKeyboardDoubleArrowRight
-        className="sidebar-toggle"
-        onClick={handleToggler}
-      />
+      <div className="sideBar-top">
+        <h2>Movies</h2>
+        <MdKeyboardDoubleArrowRight
+          className={isExpanded ? "sidebar-toggle rotated" : "sidebar-toggle"}
+          onClick={handleToggler}
+        />
+      </div>
+
+      <nav className={isExpanded ? "links" : "links icons-only"}>
+        <div>
+          <AiOutlineHome />
+          <h4>Home</h4>
+        </div>
+        <div>
+          <AiOutlineFire />
+          <h4>Trending</h4>
+        </div>
+        <div>
+          <TbMovie />
+          <h4>Movies</h4>
+        </div>
+        <div>
+          <SlScreenDesktop />
+          <h4>Shows</h4>
+        </div>
+      </nav>
     </div>
   );
 };
