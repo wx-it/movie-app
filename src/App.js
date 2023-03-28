f5645fe59da4c21a5221fda5afa2add197cefd0c
Lix <zinebbendjafer9@gmail.com>
2023-03-28 22:02:11 +0100

    added main title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch(
-        "https://api.themoviedb.org/3/trending/all/day?api_key=305075112da051598dad6f3e8103590b"
+        "https://api.themoviedb.org/3/discover/movie?api_key=305075112da051598dad6f3e8103590b"
       );
 
       const response = await data.json();
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Movies App</h1>
+        <h1>Movies Planet</h1>
       </header>
       <main>
         <Movie movies={movies} />
