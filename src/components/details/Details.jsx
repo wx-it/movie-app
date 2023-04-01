15969ba10332bb93fb55550cd8e55a2eca94bf2c
Lix <zinebbendjafer9@gmail.com>
2023-04-01 22:25:26 +0100

    get cast from imdb done

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,7 +1,22 @@
 import "./details.css";
+import { useEffect, useState } from "react";
 
 const Details = ({ find }) => {
+
+  const [cast, setCast] = useState()
   const getGenres = find.genres.map((g) => <p key={g.name}>{g.name}</p>);
+  const getPrCompanies = find.production_companies.map(prC=> <p key={prC.name}> {prC.name} </p>)
+
+  //get cast from imdb api
+  useEffect(()=>{
+    const fetchImdbCast= async () =>{
+      const response = await fetch(`https://imdb-api.com/en/API/FullCast/k_apmw1kwy/${find.imdb_id}`)
+      const data = await response.json()
+      setCast(data)
+    }
+    fetchImdbCast()
+  },[])
+  console.log(cast)
 
   return (
     <div className="about">
@@ -16,6 +31,7 @@ const Details = ({ find }) => {
         </div>
       </div>
       <h1> {find.title} </h1>
+      <span> {find.release_date} </span>
       <p> {find.overview} </p>
     </div>
   );
